9d2da2150368aa44a70bbc8f8e2df2c7caba7e00
yonhasky <yonhasky@naver.com>
2022-02-06 17:45:01 +0900

    springSecurity

--- Restful-WebService/src/main/java/com/example/RestfulWebService/config/SwaggerConfig.java
+++ Restful-WebService/src/main/java/com/example/RestfulWebService/config/SwaggerConfig.java
@@ -13,9 +13,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
 
 	// http://localhost:8089/swagger-ui/index.html
